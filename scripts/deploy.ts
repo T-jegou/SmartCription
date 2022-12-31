aa663fcaea89cb6d273fcc22d1eca68553916727
azerpas <xskylix@hotmail.fr>
2022-12-31 17:40:51 +0100

    feat: deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,24 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    if (!process.env.MEDIC_ADDRESS) throw new Error("MEDIC_ADDRESS not set")
+    if (!process.env.PHARMA_ADDRESS) throw new Error("PHARMA_ADDRESS not set")
 
-  const lockedAmount = ethers.utils.parseEther("1");
+    const medic = process.env.MEDIC_ADDRESS
+    const pharma = process.env.PHARMA_ADDRESS
+    console.log(`Medic address: ${medic}`)
+    console.log(`Pharma address: ${pharma}`)
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+    const SmartCription = await ethers.getContractFactory("SmartCription")
+    let smartCription = await SmartCription.deploy()
+    await smartCription.deployed()
+    await smartCription.grantRole(await smartCription.MEDIC_ROLE(), medic)
+    await smartCription.grantRole(await smartCription.PHARMACIST_ROLE(), pharma)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
