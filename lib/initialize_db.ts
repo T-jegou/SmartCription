d442f653621a3c998eb5357674d757224b663ce8
mitra.hejazipour <mitra.hejazipour@polynom.io>
2023-02-26 12:40:13 +0100

    move to types

--- lib/initialize_db.ts
+++ lib/initialize_db.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import { Database } from './database.types'
+import { Database } from './types/database.types'
 
 
 export async function connectionDB() {
