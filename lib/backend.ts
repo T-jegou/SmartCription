4c42125467a382013fdd889148ce616c682f77c7
azerpas <xskylix@hotmail.fr>
2023-02-26 15:56:23 +0100

    feat: new field prescription hash for prescription in supabase

--- lib/backend.ts
+++ lib/backend.ts
@@ -2,67 +2,64 @@ import { connectionDB } from "./initialize_db";
 import { v4 as uuidv4 } from 'uuid';
 
 
-export async function userPrescription(prescriptionId: number, patientAdress: string) {
-  const supabase = await connectionDB()
-  const { error } = await supabase
-    .from('userToken')
-    .insert({ id: uuidv4(), prescriptionId: prescriptionId, address: patientAdress })
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('Data inserted successfully!');
-  }
+export async function addPrescription(prescriptionId: number, patientAdress: string, prescriptionHash: string) {
+    const supabase = await connectionDB()
+    const { error } = await supabase
+        .from('userToken')
+        .insert({ id: uuidv4(), prescriptionId: prescriptionId, address: patientAdress, prescriptionHash })
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('Data inserted successfully!');
+    }
+}
+
+
+export async function getPrescription(prescriptionId: number) {
+    const supabase = await connectionDB()
+
+    const { data, error } = await supabase
+        .from('userToken')
+        .select().eq('prescriptionId', prescriptionId)
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('Data inserted successfully!');
+    }
+    return data;
+}
+
+export async function deletePrescription(prescriptionId: number) {
+    const supabase = await connectionDB()
+
+    const { error } = await supabase
+        .from('userToken')
+        .delete()
+        .eq('prescriptionId', prescriptionId)
+    if (error) {
+        console.error(error);
+        return false;
+    } else {
+        console.log('Data deleted successfully!');
+        return true;
+    }
 
 }
 
 
-export async function getUserPrescription(prescriptionId: number) {
-  const supabase = await connectionDB()
+export async function updatePrescription(prescriptionId: string, patientAddress: string) {
+    const supabase = await connectionDB()
 
-  const { data, error } = await supabase
-    .from('userToken')
-    .select().eq('prescriptionId', prescriptionId)
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('Data inserted successfully!');
-  }
-  return data;
-}
-
-
-
-export async function deleteUserPrescription(prescriptionId: number) {
-  const supabase = await connectionDB()
-
-  const { error } = await supabase
-    .from('userToken')
-    .delete()
-    .eq('prescriptionId', prescriptionId)
-  if (error) {
-    console.error(error);
-    return false;
-  } else {
-    console.log('Data deleted successfully!');
-    return true;
-  }
-
-}
-
-
-export async function updateUserPrescription(prescriptionId: string, patientAddress: string) {
-  const supabase = await connectionDB()
-
-  const { error } = await supabase
-    .from('userToken')
-    .update({ prescriptionId: prescriptionId })
-    .eq('Address', patientAddress)
-  if (error) {
-    console.error(error);
-    return false;
-  } else {
-    console.log('Data updated successfully!');
-    return true;
-  }
+    const { error } = await supabase
+        .from('userToken')
+        .update({ prescriptionId: prescriptionId })
+        .eq('Address', patientAddress)
+    if (error) {
+        console.error(error);
+        return false;
+    } else {
+        console.log('Data updated successfully!');
+        return true;
+    }
 
 }
--- lib/types/database.types.ts
+++ lib/types/database.types.ts
@@ -2,7 +2,7 @@ export interface Database {
     public: {
       Tables: {
         userToken: {
-          Row: {id: 'uuid',prescriptionId:number, address: string} 
+          Row: {id: 'uuid', prescriptionId: number, address: string, prescriptionHash: string} 
           Insert: {} 
           Update: {} 
         }
