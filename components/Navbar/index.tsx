2a15a73a15c86924ff853906eb635e6598f478ee
fannihalmai <fanni.halmai@efrei.net>
2023-03-01 16:38:58 +0100

    add logo to toolbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,13 +1,23 @@
 import { Container, Group } from "@mantine/core";
 import SelectWalletModal from "@components/wallet/ModalConnect"
 import { useWeb3React } from "@web3-react/core"
+import Icon from '@mdi/react';
+import { mdiAmbulance } from '@mdi/js';
 
 export default function Navbar() {
     
     return (
         <Container pb={0} fluid={true}>
             <Group position="apart" sx={{ height: '100%' }}>
-                <h1>Smartcription</h1>
+            <div>
+            
+            <h1>
+                <Icon className="logo" path={mdiAmbulance}
+                size={1.5}
+                color="#684DEC"
+              
+            />Smartcription {process.env.PUBLIC_URL}</h1>
+            </div>
                 <SelectWalletModal/>
             </Group>
         </Container>
